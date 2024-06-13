718a47f1dfee7d7ca156e4743c9143724b9d4737
Michael Chen <machen27@gmail.com>
2024-06-12 22:54:43 -0400

    Remove homerow mod shift

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/machen/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/machen/keymap.c
@@ -15,22 +15,25 @@
 
  Modifications by Michael Chen, June 2023 following (https://precondition.github.io/home-row-mods#getting-started-with-home-row-mods-on-qmk)
  Setting up MOD-TAP as a layer instead of a MAC layer. Reminder that the front switch swaps between the MAC (in this case, homerow mod) layers and a base layer
+
+ Remember to compile using QMK MSYS and
+ qmk compile -kb keychron/k8_pro/ansi/rgb -km machen
  */
 
 #include QMK_KEYBOARD_H
 
 
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LSFT_T(KC_D)
+//#define HOME_A LGUI_T(KC_A)
+#define HOME_S LGUI_T(KC_S)
+#define HOME_D LALT_T(KC_D)
 #define HOME_F LCTL_T(KC_F)
 
 // Right-hand home row mods
 #define HOME_J RCTL_T(KC_J)
-#define HOME_K RSFT_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SCLN RGUI_T(KC_SCLN)
+#define HOME_K LALT_T(KC_K)
+#define HOME_L LGUI_T(KC_L)
+//#define HOME_SCLN RGUI_T(KC_SCLN)
 
 // Define shift-tab
 #define M_BKTB LSFT(KC_TAB)
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,           KC_SNAP,   KC_CAPS,  RGB_MOD,
      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,    KC_HOME,  KC_PGUP,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,    KC_END,   KC_PGDN,
-     M_BKTB,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_QUOT,            KC_ENT,
+     M_BKTB,  KC_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,     KC_H,     HOME_J,   HOME_K,   HOME_L,   KC_SCLN,KC_QUOT,            KC_ENT,
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
      KC_LCTL,  KC_LGUI,  KC_LALT,                               KC_SPC,                                  KC_RALT,  KC_RGUI, MO(MAC_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
